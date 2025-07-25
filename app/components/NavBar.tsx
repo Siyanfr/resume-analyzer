4b40746bb34c8bdf814bd4d22a35f7af13cc48d2
Siyanfr <carosuscean@yahoo.com>
2025-07-25 22:34:47 +0800

    Update

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,14 @@
+import {Link} from "react-router";
+
 const NavBar = () => {
     return (
         <nav className="navbar">
-
+            <Link to="/">
+                <p className="text-2xl font-bold">Talentra</p>
+            </Link>
+            <Link to="/upload">
+                Upload Resume
+            </Link>
         </nav>
     );
 };
